228fb19a703d7c3e0e565564ad1c232091330eae
adirozeri <adirozeri@gmail.com>
2024-12-09 11:52:43 +0200

    removessl

--- run.py
+++ run.py
@@ -411,9 +411,9 @@ if __name__ == '__main__':
     app.run(
         host='0.0.0.0', 
         port=8000,
-        ssl_context=(
-            'static/assets/fullchain.pem',
-            'static/assets/privkey.pem'
-        ),
+        # ssl_context=(
+        #     'static/assets/fullchain.pem',
+        #     'static/assets/privkey.pem'
+        # ),
         debug=args.debug
     )
