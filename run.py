3bc8271b6efa14d5db647a93a3e6f7d06823db2c
adirozeri <adirozeri@gmail.com>
2024-12-16 17:07:38 +0200

    SSL_DIRECT

--- run.py
+++ run.py
@@ -523,21 +523,9 @@ if __name__ == '__main__':
     }
     
     # Add SSL context only if ENV is development
-    if os.environ.get('FLASK_ENV') != 'development':
+    if os.environ.get('SSL_DIRECT') == 'true':
         config['ssl_context'] = (
             'static/assets/fullchain.pem',
             'static/assets/privkey.pem'
         )
-    #
     app.run(**config)
-
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def hello():
-#     return "Hello World!"
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=8000)
